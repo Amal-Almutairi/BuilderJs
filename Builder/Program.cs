0b1598e50c9c96f7d84ec3ae216714d833e5a4c3
amalFreeh1 <amal.almotairi@tuwaiq.edu.sa>
2021-04-19 04:42:07 +0300

    Homework 2 Builder js

---
+++ Builder/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Builder
+{
+    class Builder
+    {
+        private string js;
+        public Builder()
+        {
+            this.js = "";
+        }
+        public Builder beginTag(string value)
+        {
+            this.js += "<" + value + ">";
+            return this;
+        }
+        public Builder setContent(string value)
+        {
+            this.js += "document.write(" + value + ");";
+            return this;
+        }
+        public Builder endTag(string value)
+        {
+            this.js += "</" + value + ">";
+            return this;
+        }
+        public string get()
+        {
+            return this.js;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Builder js = new Builder();
+            string code = js.beginTag("script")
+                .setContent("5")
+                .endTag("script").get();
+            Console.WriteLine("\n" + code);
+        }
+    }
+}
